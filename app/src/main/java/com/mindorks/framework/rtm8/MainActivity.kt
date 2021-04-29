2d10c952719e9d358157d9331c31485b6a1ced75
louisaldorio <louisaldorio@gmail.com>
2021-04-29 19:57:13 +0700

    SoundPool

--- app/src/main/java/com/mindorks/framework/rtm8/MainActivity.kt
+++ app/src/main/java/com/mindorks/framework/rtm8/MainActivity.kt
@@ -1,10 +1,17 @@
 package com.mindorks.framework.rtm8
 
+import android.annotation.TargetApi
+import android.media.AudioManager
+import android.media.SoundPool
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
+
+var sp : SoundPool? = null
+var soundID  : Int = 0
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
     //anda bisa cek datanya di Device Explorer >> data >> data >> {nama package anda di baris 1} >> shared_prefs
@@ -28,6 +35,11 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
                 mySharedHelper.email = edit_text_email.text.toString()
                 Toast.makeText(this,"Simpan berhasil", Toast.LENGTH_SHORT).show()
                 clearEditText()
+
+                //play sound
+                if(soundID!=0) {
+                    sp?.play(soundID,.99f,.99f,1,0,.99f)
+                }
             }
             R.id.btn_reset -> {
                 mySharedHelper.clearValues()
@@ -42,5 +54,40 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
     private fun clearEditText(){
         edit_text_name.text.clear()
         edit_text_email.text.clear()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP)
+            createNewSoundPool()
+        else
+            createOldSoundPool()
+
+        sp?.setOnLoadCompleteListener{soundPool, id, status ->
+            if(status != 0)
+                Toast.makeText(this,"Gagal Load",Toast.LENGTH_SHORT)
+                        .show()
+            else
+                Toast.makeText(this,"Load Sukses",Toast.LENGTH_SHORT)
+                        .show()
+        }
+        soundID = sp?.load(this, R.raw.hoodini,1) ?: 0
+
+    }
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    private fun createNewSoundPool() {
+        sp = SoundPool.Builder()
+                .setMaxStreams(15)
+                .build()
+    }
+    @Suppress("DEPRECATION")
+    private fun createOldSoundPool() {
+        sp = SoundPool(15, AudioManager.STREAM_MUSIC,0)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        sp?.release()
+        sp = null
     }
 }
